86a6f4cfc7fd45860ecb966d92f368f785fa0863
pryaniki <skoro11@bk.ru>
2022-07-28 19:39:56 +0300

    Рабочая версия

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import requests
 from bs4 import BeautifulSoup as BS
 
 
-def read_page(url, counter, c):
+def read_page(url, counter):
 
     request = requests.get(url)
-    if request.status_code == 200:
-      #print('Success!')
-        pass
-    elif request.status_code == 404:
-      print('Not Found.')
+    if request.status_code != 200:
+        print('Не удалось попасть на страницу')
+        return False, {}
 
     html = BS(request.content, 'html.parser')
     url = None
@@ -23,13 +21,8 @@ def read_page(url, counter, c):
 
     for el in html.find(id="mw-pages").select('[class~=mw-category-group]'):
       char = el.h3.contents[0].upper()
-      if char in counter.keys() and char == c:
-          if char == c:
-              #counter[char] += len(el.ul.find_all("li"))
-              for name in el.ul.find_all("li"):
-                  counter[char] += 1
-                  print(f'{counter[char]} {name.a.contents[0]}')
-
+      if char in counter.keys():
+          counter[char] += len(el.ul.find_all("li"))
       else:
           url = None
           break
@@ -37,33 +30,22 @@ def read_page(url, counter, c):
     return url, counter
 
 
-def get_count_animals(url, ct):
+def get_count_animals(url):
 
     letters = [(lambda c: chr(c))(i) for i in range(1040, 1072)]
     counter = {}
     for c in letters:
         counter[c] = 0
-    url, counter = read_page(url, counter, ct)
-    while url:
-        url, counter = read_page(url, counter, ct)
-        pass
+    url, counter = read_page(url, counter)
 
+    while url:
+        url, counter = read_page(url, counter)
     return counter
+
 
 def main():
     url = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83'
-    #url = 'https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%96%D0%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83&pagefrom=%D0%AF%D1%89%D0%B5%D1%80%D0%B8%D1%87%D0%BD%D1%8B%D0%B5+%D0%B7%D0%BC%D0%B5%D0%B8&subcatfrom=%D0%AF&filefrom=%D0%AF#mw-pages'
-    import urllib.parse
-    letters = [(lambda c: chr(c))(i) for i in range(1040, 1072)]
-    #counter = get_count_animals(url)
-    for c in letters:
-        url = 'https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F%3A%D0%96%D0%B8%D0%B2%D0%BE%D1%82%D0%BD%D1%8B%D0%B5_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83&from='
-        url += urllib.parse.quote(c)
-        counter = get_count_animals(url, c)
-        for ca in counter:
-            print(f'{ca}: {counter[ca]}')
-        print()
-        print()
+    counter = get_count_animals(url)
     for c in counter:
          print(f'{c}: {counter[c]}')
 
